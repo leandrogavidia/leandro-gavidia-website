6d1364d73d7f1e833190cc63bef5af1aba6cf044
Leandro Gavidia <leandro.d3v@gmail.com>
2025-12-10 15:06:29 -0400

    fix: update email address and remove unused social media entries from footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="w-full flex flex-col justify-center items-center">
       <section className="flex flex-col items-center justify-center w-full gap-5  rounded-lg p-8 text-center">
         <address className="text-lg sm:text-xl">
-          <span className="font-semibold">Email:</span> contact@leandrogavidia.com
+          <span className="font-semibold">Email:</span> leandro.d3v@gmail.com
         </address>
       </section>
     </div>
--- components/footer/item-list/index.ts
+++ components/footer/item-list/index.ts
@@ -5,21 +5,6 @@ export const itemList = [
         icon: "/images/social-media-icons/medium.svg"
     },
     {
-        name: "Facebook",
-        url: "https://www.facebook.com/androgavidia",
-        icon: "/images/social-media-icons/facebook.svg"
-    },
-    {
-        name: "Instagram",
-        url: "https://www.instagram.com/androgavidia/",
-        icon: "/images/social-media-icons/instagram.svg"
-    },
-    {
-        name: "Threads",
-        url: "https://www.threads.net/@androgavidia",
-        icon: "/images/social-media-icons/threads.svg"
-    },
-    {
         name: "X",
         url: "https://twitter.com/androgavidia",
         icon: "/images/social-media-icons/x.svg"
